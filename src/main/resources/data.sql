4a748faeb34a2f1352c85e55c623b4b03bd7a76c
Israel Andrade <israelg.andrade@bol.com.br>
2019-07-09 15:17:43 -0300

    Adicionando arquivo .sql para inserir dados no BD para teste - opcional

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+//insert into TBL_PRODUTO	values(null,'Imagem de satélites de qualquer cidade','SATMAPS');
+
+//insert into TBL_PRODUTO	values(null,'Imagem de desmatamento do estado de Amazonas','DESMAPS');
+
+//insert into TBL_IMAGEM	values(null,'/home/store/desmatamento','DESMAPS - AREA 1', 2);
+//insert into TBL_IMAGEM	values(null,'/home/store/desmatamento','DESMAPS - AREA 2', 2);
+//insert into TBL_IMAGEM	values(null,'/home/store/desmatamento','DESMAPS - AREA 3', 2);
+
+//insert into TBL_IMAGEM	values(null,'/home/store/satelite','SATMAPS - ZONA 1', 1);
+//insert into TBL_IMAGEM	values(null,'/home/store/satelite','SATMAPS - ZONA 2', 1);
